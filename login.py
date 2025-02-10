cfce8333514256802ffae3bd6ba68de0004b5611
SylviaGit4 <torchicky139@gmail.com>
2025-02-10 15:16:12 +0000

    Password & username confirmation

--- login.py
+++ login.py
@@ -4,7 +4,27 @@ import pandas as pd
 def user_login(username, password):
     data = pd.read_csv("users.csv")
 
-    user_name = username
-    user_pass = password
+    user_correct = False
+    pass_correct = False
 
-    print(data)
+    if username not in data["Username"].values:
+        print("Invalid Username")
+        while True:
+            choice = input("Register a new account or try again [R/T]: ").upper()[0]
+            if choice == "R":
+                pass
+            if choice == "T":
+                break
+            else:
+                print("Invalid selection")
+    else:
+        print("Valid Username")
+        user_correct = True
+
+    if password not in data["Password"].values:
+        print("Invalid Password, try again.")
+    else:
+        print("Valid Password")
+        pass_correct = True
+
+user_login("test","test")
