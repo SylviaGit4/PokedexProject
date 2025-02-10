24a2ab736aa98a744ae7f49e2b3732b482fb7f97
SylviaGit4 <torchicky139@gmail.com>
2025-02-10 14:56:57 +0000

    Prepared pandas

--- login.py
+++ login.py
@@ -1,6 +1,12 @@
-
+import pandas as pd
 
 ########################
 def login(username, password):
+    data = pd.read_csv("users.csv")
+
     user_name = username
     user_pass = password
+
+    print(data)
+
+login(1,2)
