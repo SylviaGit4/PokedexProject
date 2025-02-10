f4f37b1484e4252d7d0bc78ee4ff8c1d30d9b994
SylviaGit4 <torchicky139@gmail.com>
2025-02-10 14:37:04 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import requests as rq
+import json
+
+########################
+root = tk.Tk()
+root.title("Pokedex")
+root.geometry("480x680")
+########################
+
+
+
+########################
+root.mainloop()
