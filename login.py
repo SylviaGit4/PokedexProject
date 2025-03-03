1f57e6c69ee550501ce40ef76ae1fdfc03daa1a6
SylviaAurora <98970536+SylviaGit4@users.noreply.github.com>
2025-03-03 15:27:10 +0000

    Update login.py

--- login.py
+++ login.py
@@ -1,32 +1,55 @@
 import pandas as pd
+import tkinter as tk
 
 ########################
 def user_login():
-    df = pd.read_csv("users.csv")
 
-    user_correct = False
-    pass_correct = False
+    loginWindow = tk.Tk()
+    loginWindow.title("Pokedex")
+    loginWindow.geometry("480x680")
 
-    while user_correct == False:
-        username = input("Username: ")
-        if username not in df["Username"].values:
-            print("Invalid Username")
-        else:
+    user_id = ""
+    
+    def submit(username, password):
+
+        df = pd.read_csv("users.csv")
+
+        user_correct = False
+        pass_correct = False
+
+        print(df["Username"].tolist())
+
+        print(username)
+
+        if str(username) in df["Username"].tolist():
             print("Valid Username")
             user_correct = True
-    
-    selected_row = df.loc[df["Username"] == username]
-    
-    while pass_correct == False:
-        password = input("Password: ")
-        if password != selected_row.iloc[0,2]:
-            print("Invalid Password, try again.")
-        else:
-            print("Valid Password")
-            pass_correct = True
+            selected_row = df.loc[df["Username"] == username]
+            print(selected_row)
 
-    user_id = selected_row.iloc[0,0]
+            if str(password) == selected_row.iloc[0,2] and user_correct == True:
+                print("Valid Password")
+                pass_correct = True
+
+            user_id = selected_row.iloc[0,0]
         
+        else:
+            print("Invalid Username")
+
+    def submit_start():
+        submit(username_entry.get(), password_entry.get())
+
+    username_entry = tk.Entry(loginWindow)
+    username_entry.grid(row=0,column=1)
+
+    password_entry = tk.Entry(loginWindow)
+    password_entry.grid(row=1,column=1) 
+
+    sub_btn=tk.Button(loginWindow,text = 'Submit', command = submit_start)
+    sub_btn.grid(row=2,column=1)
+
+    loginWindow.mainloop()
+
     return(user_id)
 
 user_id = user_login()
