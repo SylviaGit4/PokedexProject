c006c85518793aee00bedb79a812f7c8a5618c63
SylviaGithub <torchicky139@gmail.com>
2025-03-09 00:02:04 +0000

    Need to fix the login thing again

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ import login
 ########################
 root = tk.Tk()
 root.title("Pokedex")
-root.geometry("600x800")
 ########################
 
 #Calling the login function later
 def user_login():
     user_data = login.user_login()
-    print(user_data)
+    
+    lbl_username.config(text=user_data[0])
 ########################
 
 frm_main = tk.Frame(
     root,
     bg="SlateGray1",
-    height="400px",
-    width="400px",
+    height="400",
+    width="400",
     highlightbackground="grey27",
     highlightthickness=5,
     )
@@ -28,20 +28,22 @@ frm_main = tk.Frame(
 frm_key_right = tk.Frame(
     root,
     bg="indian red",
-    height="600px", 
-    width="100px", 
+    height="600", 
+    width="200", 
+    padx="10",
+    pady="5",
     )
 
 frm_key_bottom = tk.Frame(
     root,
     bg="indian red",
-    height="200px",
-    width="400px", 
+    height="200",
+    width="400", 
     )
 
-frm_main.grid(column=0,row=0, rowspan=1)
-frm_key_right.grid(column=1, row=0, rowspan=2)
-frm_key_bottom.grid(column=0, row=1)
+frm_main.grid(column=0,row=0, rowspan=1, sticky="ns")
+frm_key_right.grid(column=1, row=0, rowspan=2, sticky="ns")
+frm_key_bottom.grid(column=0, row=1, sticky="ns")
 
 btn_login = tk.Button(
     master=frm_key_right,
@@ -49,9 +51,19 @@ btn_login = tk.Button(
     command=lambda:user_login(),
     bg="firebrick3",
     fg="white",
+    padx="5",
 )
 
-btn_login.grid(column=0, row=0)
+lbl_username = tk.Label(
+    master=frm_key_right,
+    text="USERNAME",
+    bg="firebrick3",
+    fg="white",
+    padx="5",
+)
+
+lbl_username.grid(column=0, row=0)
+btn_login.grid(column=0, row=1)
 
 ########################
 root.mainloop()
