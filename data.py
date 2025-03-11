8214e00361282a9704fea01154133e6ef957ab4e
SylviaGit4 <torchicky139@gmail.com>
2025-03-11 14:59:06 +0000

    Saving

--- data.py
+++ data.py
@@ -22,7 +22,6 @@ def poke_search(entry):
 def poke_lookup(poke_data):
     info_window = tk.Toplevel()
     info_window.title("Pokemon Inf")
-    info_window.geometry("200x100")
 
     info_window.columnconfigure(0, weight=1)
     info_window.columnconfigure(1, weight=1)
@@ -30,14 +29,18 @@ def poke_lookup(poke_data):
 
     # Data
     poke_id = (poke_data['id'])
-    poke_name = (poke_data['name']).title()
+    poke_name = (poke_data['name']).title()                                                                     
+    poke_type = [typ["type"]["name"] for typ in poke_data["types"]]
+    print(len(poke_type))
+    #if len(poke_type) == 1:
+    #    poke_type_text = f"Types: {poke_type[1]}"
+    #else:
+    #    poke_type_text = f"Types: {poke_type[1], poke_type[2]}"
 
 
     frm_info_main = tk.Frame(
         master=info_window,
         bg="indian red",
-        height=75,
-        width=200,
         highlightbackground="grey27",
         highlightthickness=5,
     )
@@ -55,8 +58,8 @@ def poke_lookup(poke_data):
 
     lbl_name = tk.Label(
         master=frm_info_main,
-    text=f"Pokemon: {poke_name}",
-        bg="firebrick3",
+        text=f"Pokemon: {poke_name}",
+        bg="indian red",
         fg="white",
         padx="5",
     )   
@@ -64,14 +67,23 @@ def poke_lookup(poke_data):
     lbl_id = tk.Label(
         master=frm_info_main,
         text=f"ID: {poke_id}",
-        bg="firebrick3",
+        bg="indian red",
+        fg="white",
+        padx="5",
+    )   
+
+    lbl_types = tk.Label(
+        master=frm_info_main,
+        #text=poke_type_text,
+        bg="indian red",
         fg="white",
         padx="5",
     )   
 
 
-    sub_btn.grid(row=0,column=0)
+    sub_btn.grid(row=0,column=0, pady=3)
     lbl_name.grid(row=1,column=0)
     lbl_id.grid(row=1,column=1)
+    lbl_types.grid(row=2,column=0, columnspan=2)
 
     info_window.mainloop()
